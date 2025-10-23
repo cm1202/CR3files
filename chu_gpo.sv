2534e4c62ca274b88520f6f84783ea21217e0bf9
cm1202 <96462904+cm1202@users.noreply.github.com>
2025-10-22 22:09:53 -0500

    Update chu_gpo.sv

--- chu_gpo.sv
+++ chu_gpo.sv
@@ -1,37 +1,56 @@
 module chu_gpo
-   #(parameter W = 8)  // width of output port
+    #(parameter W = 8)
    (
     input  logic clk,
     input  logic reset,
-    // slot interface
     input  logic cs,
     input  logic read,
     input  logic write,
     input  logic [4:0] addr,
     input  logic [31:0] wr_data,
     output logic [31:0] rd_data,
-    // external port    
     output logic [W-1:0] dout
    );
 
-   // declaration
-   logic [W-1:0] buf_reg;
-   logic wr_en;
+   localparam int ADDR_DATA  = 5'h00;
+   localparam int ADDR_SPEED = 5'h01;
 
-   // body
-   // output buffer register
-   always_ff @(posedge clk, posedge reset)
+   logic [W-1:0] buf_reg;      // LED pattern
+   logic [31:0] speed_reg;     // clock cycles
+   logic blink_sig; 
+   logic [31:0] blink_counter; // cycle counter
+   logic blink_state;          
+   logic wr_en_data;
+   logic wr_en_speed;
+
+   assign wr_en_data  = cs && write && (addr == ADDR_DATA);
+   assign wr_en_speed = cs && write && (addr == ADDR_SPEED);
+
+   always_ff @(posedge clk, posedge reset) begin
       if (reset)
-         buf_reg <= 0;
-      else   
-         if (wr_en)
-            buf_reg <= wr_data[W-1:0];
-   // decoding logic 
-   assign wr_en = cs && write;
-   // slot read interface
-   assign rd_data =  0;
-   // external output  
-   assign dout = buf_reg;
+         buf_reg <= '0;
+      else if (wr_en_data)
+         buf_reg <= wr_data[15:0];
+   end
+
+   // speed 
+   always_ff @(posedge clk, posedge reset) begin
+      if (reset)
+         speed_reg <= 0;
+      else if (wr_en_speed)
+         speed_reg <= wr_data;
+   end
+   
+   assign rd_data = 0; 
+
+   blink_controller blink0(
+    .clk(clk),
+    .rst(reset),
+    .speed(speed_reg),
+    .led(blink_sig)
+    ); 
+    
+    assign dout = buf_reg & {W{blink_sig}}; 
 endmodule
        
 
